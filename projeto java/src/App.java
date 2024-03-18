6409bc20186a0858896778e5eae6d217db4d45d3
JonathaCoder <jonatha2519@gmail.com>
2024-03-18 13:47:22 -0300

    Aula Java

---
+++ projeto java/src/App.java
@@ -0,0 +1,48 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //variaveis do Tipo inteiro
+       byte numeroPequeno;
+       short numeroShort;
+       int numeroMaior;
+       long numeroBig;
+ 
+
+       //variaveis do tipo Ponto flutuante
+    float peso;
+    double pi;
+    char letra;
+    boolean fome;
+
+
+       letra = 'C';
+       pi = 20.3232332323;
+       peso = 30.6f;
+       fome = true;
+
+
+
+
+
+  // Inteiros
+       numeroShort = 32766;
+       numeroPequeno = 20;
+       numeroMaior = 214748680;
+       numeroBig = 922337203685477000l;
+       
+ //String 
+ String nome = "jonatha";
+
+    System.out.println(numeroPequeno);
+    System.out.println(numeroShort);
+    System.out.println(numeroMaior);
+    System.out.println(numeroBig);
+
+
+
+    System.out.println(peso);
+    System.out.println(pi);
+    System.out.println(fome);
+    System.out.println(nome);
+ 
+    }
+}
